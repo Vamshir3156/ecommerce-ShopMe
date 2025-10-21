c18f569413b759b0fd7198e26a69d90596669284
Vamshi Ratna <vamshiratna@Vamshis-MacBook-Air-2.local>
2025-10-21 00:01:07 -0700

    Updated lib_api.js to dynamically detect VITE_API_URL

--- client/src/lib_api.js
+++ client/src/lib_api.js
@@ -1,3 +1,12 @@
-import axios from 'axios'
-const api = axios.create({ baseURL:'http://localhost:5000/api', withCredentials:true })
-export default api
+import axios from "axios";
+
+const API_BASE = (
+  import.meta.env.VITE_API_URL || "http://localhost:5000"
+).replace(/\/$/, "");
+
+const api = axios.create({
+  baseURL: `${API_BASE}/api`,
+  withCredentials: true,
+});
+
+export default api;
